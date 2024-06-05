fd6b6f765c7f59bee7c8b7d2921da7863e8a7521
H04411 <H04411@lab.aca>
2024-06-05 16:13:31 -0500

    post put delete

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,49 @@ app.get("/publicaciones/:id", function(req, res) {
 
 })
 
+app.post("/publicaciones", function(req, res) {
+    const data = req.body;
+    if(data && data.titulo && data.contenido && data.autor){
+        const nuevoId = publicaciones.length + 1;
+        const nuevaPublicacion = crearPublicacion(nuevoId, data.titulo, data.contenido, data.autor);
+        publicaciones.push(nuevaPublicacion);
+        res.json(nuevaPublicacion);
+        console.log(nuevaPublicacion);
+    } else {
+        res.status(404).send("Faltan datos")
+    };
+})
+
+app.put("/publicaciones/:id", function(req, res) {
+    const id = req.params.id;
+    const data = req.body;
+
+    if(data.titulo && data.contenido && data.autor){
+        const publicacion = publicaciones.find((pub) => pub.id == id);
+        if(publicacion) {
+            publicacion.titulo = data.titulo;
+            publicacion.contenido = data.contenido;
+            publicacion.autor = data.autor;
+            res.json(publicacion);
+        } else {
+            res.status(404).send("no se encontró la publicacion");
+        }
+    } else {
+        res.status(404).send("faltan datos");
+    }
+})
+
+app.delete("/publicaciones/:id", function(req, res){
+    const id = req.params.id;
+    const publicacion =  publicaciones.find((pub) => pub.id == id);
+    if(publicacion) {
+        publicacion.estado = "eliminado";
+        res.json(publicacion);
+    } else {
+        res.status(404).send("no se encontró la publicacion");
+    }
+})
+
 app.get("/publicaciones-url", function(req, res) {
     const id = req.query.id;
     const publicacion =  publicaciones.find((pub) => pub.id == id);
